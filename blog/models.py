643396041c2bba55df905e45e0ade643f65beeec
eunchae <eunchae@DESKTOP-A9VFHBK>
2019-08-07 14:46:34 +0900

    blog앱의 models에 내용추가
    -Comment에 평점 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,21 @@ from django.db import models
 
 
 #Comment필드는 ('post','작성자', '내용','평점')
-#class Comment(models.Model):
+class Comment(models.Model):
+
+    
+    grade_total= (
+    (5, '5점'),
+
+    (4, '4점'),
+
+    (3, '3점'),
+
+    (2, '2점'),
+
+    (1, '1점'),
+    )
+    평점 = models.FloatField(
+        choices=grade_total,
+        default= 5
+    )
