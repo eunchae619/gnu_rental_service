d59f6597351e4bf6381dec4650340b18a3d7581a
eunchae <eunchae@DESKTOP-A9VFHBK>
2019-08-07 15:03:39 +0900

    blog앱의 views에 detail함수 추가

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Portfolio, Comment
+from django.db.models import Avg
 # Create your views here.
 
 
@@ -24,7 +25,17 @@ from django.shortcuts import render
 
 
 #detail.html에서 평균평점 보여주는 key값 = "average"
-#def detail(request, blog_id):
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Portfolio, pk=blog_id)
+
+    a = Comment.objects.filter(post=blog_id).aggregate(Avg('평점'))
+    b = a['평점__avg']
+    if b is None:
+        average = 0
+    else:
+        average = round(b,1) 
+
+    return render(request, 'detail.html', {'blog': blog_detail,"average":average})
 
 
 #def comment(request,post_id):
